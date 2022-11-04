59b05c3c7208391d783ecd8cbebac6cec0060d51
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-11-04 09:24:05 +0530

    H

--- CKY/main.py
+++ CKY/main.py
@@ -14,14 +14,26 @@ def printRules(l):
 
 # firstly there are two types of states and these are terminals and non terminals
 # non terminals 
-nonTerminals=["S","NN","VBP","VP"]
 
 # here we are going to use the the list and the tupple in order to store the results and
 # in further step we also going to print it 
-l=[("S","NP","VP"),("NP","PP","AP"),("NN","VBP")]
+# here i will store all nonTerminals to nonTerminals rules 
+ntRules=[("S","NP","VP"),("S","X1",'VP'),('X1','AUX','NP'),('S','VERB','NP'),('S','X2','PP'),
+('S','VERB','PP'),('S','VP','PP'),('NP','DET','NOMIAL'),('NOMIAL','NOMIAL','NOUN'),('NOMIAL','NOMIAL','PP'),
+('VP','VERB','NP'),('VP','X2','PP'),('X2','VERB','NP'),('VP','VERB','PP'),('VP','VP','PP')]
 
-printRules(l)
+tRules=[('NP','i'),('NP',"she"),('NP',"me"),('NP',"twa"),("NP",'houston'),('NOMIAL',"book"),('NOMIAL',"flight")
+,('NOMIAL',"book"),('NOMIAL',"meal"),('NOMIAL',"money"),('VP',"include"),('VP',"prefer"),("VP","book"),
+("DET","the"),("DET","a"),("DET","an")]
+printRules(tRules)
 
 # so lets declare some grammer and for that we will go for the cky algo implementation
 
 
+def ckyAlgorithm(s):
+    s
+
+# NP → I | she | me
+# NP → TWA | Houston
+# Nominal → book | flight | meal | money
+# VP → book | include | prefer
